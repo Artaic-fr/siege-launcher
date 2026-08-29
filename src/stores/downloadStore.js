c09e9dc9289a8963fe593db51981433642b40bbc
Artaic <62052170+Artaic-fr@users.noreply.github.com>
2026-08-29 19:34:21 +0200

    Y4 Content added + Void Edge Content

--- src/stores/downloadStore.js
+++ src/stores/downloadStore.js
@@ -12,7 +12,7 @@ export var Launched = ref(null)
 export var reconnectionRequired = ref(false)
 export var reconnectionJobId = ref(null)
 
-const RainbowSix = ['Y1S0','Y1S1', 'Y1S2', 'Y1S2', 'Y2S2', 'Y2S3', 'Y2S4', 'Y3S1', 'Y3S2', 'Y3S3', 'Y3S4']
+const RainbowSix = ['Y1S0','Y1S1', 'Y1S2', 'Y2S2', 'Y2S3', 'Y2S4', 'Y3S1', 'Y3S2', 'Y3S3', 'Y3S4', 'Y4S1', 'Y4S2', 'Y4S3', 'Y4S4', 'Y5S1']
 
 export function initDownloadListeners() {
 
